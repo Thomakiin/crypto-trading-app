0c6886d9c34f84454de1359748d5c745ea7ffaaa
Thomakiin <Thomakiin@gmail.com>
2021-07-27 10:28:44 -0400

    fix typo in import BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import './App.css';
 import Home from './Home';
-import { BorwserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <Router>
-      <div className="App">
         <Switch>
           <Route path="/">
             <Home />
           </Route>
         </Switch>
-      </div>
     </Router>
   );
 }
