43126e6595e79b49ca91f80b19423747fdc3d412
Thomakiin <Thomakiin@gmail.com>
2021-07-27 10:25:51 -0400

    implement React Router with home page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
+import Home from './Home';
+import { BorwserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <h1>App</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
