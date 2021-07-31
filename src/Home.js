ac48155865107a18a987db1b65bb7bda22c552bb
Thomakiin <Thomakiin@gmail.com>
2021-07-31 16:55:21 -0400

    redirect to CoinCompare page and passing in state  after selecting a coin

--- src/Home.js
+++ src/Home.js
@@ -1,29 +1,26 @@
 import './App.css'
 import Searchbar from './components/Searchbar';
 import { useState } from 'react';
+import { useHistory } from 'react-router';
 
 function Home() {
-  let [selectedCoinData, setSelectedCoinData] = useState(null);
+  let [selectedCoin, setSelectedCoinData] = useState(null);
+  const history = useHistory();
 
   function test(coinData) {
-       setSelectedCoinData(coinData); 
-       console.log("Coins Page, selectedCoinData: ", selectedCoinData);
-      }
+    setSelectedCoinData(coinData);
+    console.log("Home: selectedCoin: ", selectedCoin);
+    if (selectedCoin !== null) {
+      history.push("/coincompare", { selectedCoin: selectedCoin });
+    }
+
+  }
 
   return (
-      <div>
-          <h1 >Home</h1>
-          <Searchbar onSelected={test} />
-          {selectedCoinData &&
-              <div>
-                  <img src={selectedCoinData.image.small} alt={selectedCoinData.name + " icon"} />
-                  <h1>{selectedCoinData.name}</h1>
-                  {<p>{"$" + selectedCoinData.market_data.current_price.usd + " USD"}</p>}
-
-              </div>
-          }
-
-      </div>
+    <div>
+      <h1 >Home</h1>
+      <Searchbar onSelected={test} />
+    </div>
   );
 }
 
