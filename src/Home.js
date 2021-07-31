ad0e9675c837c8c42ec588350174308c9165ef65
Thomakiin <Thomakiin@gmail.com>
2021-07-31 14:17:27 -0400

    move code from 'coins' to home

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,29 @@
 import './App.css'
+import Searchbar from './components/Searchbar';
+import { useState } from 'react';
 
 function Home() {
+  let [selectedCoinData, setSelectedCoinData] = useState(null);
+
+  function test(coinData) {
+       setSelectedCoinData(coinData); 
+       console.log("Coins Page, selectedCoinData: ", selectedCoinData);
+      }
+
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+      <div>
+          <h1 >Home</h1>
+          <Searchbar onSelected={test} />
+          {selectedCoinData &&
+              <div>
+                  <img src={selectedCoinData.image.small} alt={selectedCoinData.name + " icon"} />
+                  <h1>{selectedCoinData.name}</h1>
+                  {<p>{"$" + selectedCoinData.market_data.current_price.usd + " USD"}</p>}
+
+              </div>
+          }
+
+      </div>
   );
 }
 
