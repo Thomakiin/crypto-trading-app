56b0bc042f548bb61fc7dee53acd3187c9a6e4a8
Thomakiin <Thomakiin@gmail.com>
2021-07-31 16:53:20 -0400

    Add route for CoinCompare page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 //import './App.css';
 import Home from './Home';
 import Coins from './Coins';
+import CoinCompare from './CoinCompare';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 
 function App() {
   return (
     <BrowserRouter>
+
       <nav>
       <Link to="/"> 
         Home 
@@ -15,12 +17,16 @@ function App() {
         Coins 
         </Link>
       </nav>
+
       <Switch>
         <Route exact path="/">
           <Home />
         </Route>
         <Route exact path="/coins">
           <Coins />
+        </Route>
+        <Route exact path="/coincompare">
+          <CoinCompare />
         </Route>
       </Switch>
     </BrowserRouter >
