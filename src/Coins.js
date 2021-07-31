6fee0039b0f251a9390addc793fde183476235c6
Thomakiin <Thomakiin@gmail.com>
2021-07-31 16:56:03 -0400

    move code to coinCompare

--- src/Coins.js
+++ src/Coins.js
@@ -3,26 +3,9 @@ import Searchbar from './components/Searchbar'
 
 
 const Coins = () => {
-    let [selectedCoinData, setSelectedCoinData] = useState(null);
-
-    function test(coinData) {
-         setSelectedCoinData(coinData); 
-         console.log("Coins Page, selectedCoinData: ", selectedCoinData);
-        }
-
-    return (
+    return(
         <div>
-            <h1 >Coins</h1>
-            <Searchbar onSelected={test} />
-            {selectedCoinData &&
-                <div>
-                    <img src={selectedCoinData.image.small} alt={selectedCoinData.name + " icon"} />
-                    <h1>{selectedCoinData.name}</h1>
-                    {<p>{"$" + selectedCoinData.market_data.current_price.usd + " USD"}</p>}
-
-                </div>
-            }
-
+            <h1>Coins</h1>
         </div>
     );
 }
